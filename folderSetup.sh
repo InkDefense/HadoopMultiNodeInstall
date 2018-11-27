838a73a1acdba1dd5bd47226aa3ed05091ae2b07
Taylor <tratliff96@outlook.com>
2018-11-26 19:15:33 -0600

    Added file via upload
    common folder setup I've been using with the multi node setup so that it doesn't take as long to set up after I tear it down inbetween projects

---
+++ folderSetup.sh
@@ -0,0 +1,10 @@
+mkdir hadoop
+mkdir hadoop/tmp
+mkdir hadoop/hdfs
+mkdir hadoop/hdfs/name
+mkdir hadoop/hdfs/data
+mkdir -p hadoop/hdfs/namesecondary
+mkdir -p hadoop/nm_local_dir
+mkdir -p hadoop/mr-history/tmp
+mkdir -p hadoop/mr-history/done
+chmod -R 755 ~/hadoop
